3a62e76531be7df9b3d565533f4b369efef5f9a0
Erin <erinknowles@protonmail.com>
2023-04-01 21:35:55 -0400

    Brain can mutate genes and select a parent, parents can be sorted by distance from correct value.

--- src/Brain.js
+++ src/Brain.js
@@ -1,9 +1,15 @@
-const OPERATOR_FUNCTIONS = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => a / b,
-};
+// const OPERATOR_FUNCTIONS = {
+//     add: (a, b) => a + b,
+//     subtract: (a, b) => a - b,
+//     multiply: (a, b) => a * b,
+//     divide: (a, b) => a / b,
+// };
+// #getOperation() {
+//     const opFuncs = Object.keys(OPERATOR_FUNCTIONS);
+//     const opFuncIndex = Math.floor(Math.random() * opFuncs.length);
+//     return OPERATOR_FUNCTIONS[opFuncs[opFuncIndex]];
+// }
+// TODO: Operation is inherited and becomes a 'species' - species with unworkable operations die out.
 
 const MAX_EXPRESSION = 100;
 
@@ -12,56 +18,85 @@ class Brain {
 
     constructor() {
         this.genes = {
-            left: 0,
-            right: 0,
-            operation: this.#getOperation(),
+            left: Math.round(Math.random() * MAX_EXPRESSION),
+            right: Math.round(Math.random() * MAX_EXPRESSION),
         };
         // this.#influencability = Math.round((Math.random() * 2 - 1) * 100) / 100; // gives random between (approximately) -0.99 and +0.99
-        // console.log(
-        //     "this.genes, this.#influencability",
-        //     this.genes,
-        //     this.#influencability
-        // );
-    }
-
-    #getOperation() {
-        const opFuncs = Object.keys(OPERATOR_FUNCTIONS);
-        const opFuncIndex = Math.floor(Math.random() * opFuncs.length);
-        return OPERATOR_FUNCTIONS[opFuncs[opFuncIndex]];
-    }
-
-    pickParent(parents) {
-        // Randomly select, weighted by how correct the parent was
     }
 
     mutate(parent) {
-        // parent will have a value for left, new gene has a value for left
-        // ultimate left value should be closer to parent depending on how correct they were
-        // First, normalize
-        /*
-        Parent left: 100
-        mutation left: 0
-        parent correctness: 0.75
-        ultimate left: 75
+        if (
+            !parent ||
+            !parent.genes ||
+            parent.genes.left < 0 ||
+            parent.genes.right < 0
+        ) {
+            throw new Error("Bad parent: " + parent);
+        }
 
-        parent left: 1
-        mutation left: 100
-        parent correctness: 0.25
-        ultimate left: 75
+        function getMultiplicator() {
+            return Math.random() / 5 - 0.1 + 1; // between 1.1 and 0.99
+        }
 
-        weighted parent left: parent left * parent correctness quotient (100*0.75 = 75, 1*0.25 = 0.25)
-        ultimate left: weighted parent left - mutation left (75 - 0 = 75, 0.25
-        */
-        // ultimate operation gene should be a coin flip, weighted by how correct parent was
-
-        // TODO Error handling
-        const newGenes = {
-            left: Math.round(Math.random() * MAX_EXPRESSION),
-            right: Math.round(Math.random() * MAX_EXPRESSION),
-            operation: this.#getOperation(),
+        const genes = {
+            left: Math.round(parent.genes.left * getMultiplicator()),
+            right: Math.round(parent.genes.right * getMultiplicator()),
         };
-        const genes = (this.genes = genes);
+
+        this.genes = genes;
+        return genes;
     }
 }
 
-console.log(new Brain());
+const MY_NUMBER = 53;
+// Pick parent: given list of eligible parents, pick one to inherit from, weighted by how 'correct' they were.
+let myParents = [
+    new Brain(),
+    new Brain(),
+    new Brain(),
+    new Brain(),
+    new Brain(),
+].map((brain) => {
+    brain.distance = Math.abs(brain.genes.left + brain.genes.right - MY_NUMBER);
+    return brain;
+});
+
+function sortParents(parents) {
+    parents = parents.sort((parentA, parentB) => {
+        return parentA.distance > parentB.distance ? 1 : -1;
+    });
+    return parents;
+}
+sortParents(myParents);
+
+function pickParent(parents) {
+    // parents are sorted by distance
+    // chance of selecting a parent is logarithmic
+    // first parent option: 50% chance of selecting as parent
+    // if that fails, move to second parent (25% chance), etc
+    // once succeeds, kill the loop
+    let parent;
+    for (let i = 0; i < parents.length; i++) {
+        if (Math.random() < 1 / (2 * (i + 1))) {
+            parent = parents[i];
+            break;
+        }
+        if (i + 1 == parents.length) {
+            i = 0;
+        }
+    }
+
+    return parent;
+}
+
+const brain = new Brain();
+console.log(brain);
+
+console.log(brain.mutate(pickParent(myParents)));
+myParents.push({
+    ...brain,
+    baby: true,
+    distance: Math.abs(brain.genes.left + brain.genes.right - MY_NUMBER),
+});
+sortParents(myParents);
+console.log("parents", myParents);
